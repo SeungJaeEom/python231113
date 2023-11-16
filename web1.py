4c1a0eec39cd2ed2c5664b9775bf221e36019a14
SeungJaeEom <geniuseje@naver.com>
2023-11-16 10:30:54 +0900

    웹크롤링 연습1

---
+++ web1.py
@@ -0,0 +1,10 @@
+#web1.py
+#웹 크롤릴을 위한 선언
+from bs4 import BeautifulSoup #from 뒤는 폴더(패키지), import 뒤는 모듈(파일)
+
+#페이지 로딩
+page = open("test01.html","rt", encoding="utf-8").read()
+#검색이 용이한 객체
+soup = BeautifulSoup(page, "html.parser")
+#전체 페이지 보기
+print(soup.prettify())
