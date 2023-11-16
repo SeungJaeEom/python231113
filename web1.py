728738484d02fdbe1f49135b9ddc131f3cd202d0
SeungJaeEom <geniuseje@naver.com>
2023-11-16 11:36:41 +0900

    웹 크롤링 연습 2

--- web1.py
+++ web1.py
@@ -7,4 +7,22 @@ page = open("test01.html","rt", encoding="utf-8").read()
 #검색이 용이한 객체
 soup = BeautifulSoup(page, "html.parser")
 #전체 페이지 보기
-print(soup.prettify())
+#print(soup.prettify())
+
+#<p>전체를 검색
+#print(soup.find_all("p"))
+#첫번째<p>만 검색
+#print(soup.find("p"))
+#조건검색:<p class="outer-text">
+#class 키워드와 충돌
+#print(soup.find_all("p", class_="outer-text")) #class 자체가 파이썬의 키워드라서 class_ 라고 해준다.
+#attrs속성
+#print(soup.find_all("p", attrs={"class":"outer-text"}))
+#<p id="first">인거 찾기
+#print(soup.find_all("p", id="first"))
+#태그 안쪽의 컨텐츠만 출력: .text 속성
+for tag in soup.find_all({"p","href"}):
+    title = tag.text.strip()
+    title = title.replace("\n","")
+    title = title.replace("                                ","\n")
+    print(title)
---
+++ web2.py
@@ -0,0 +1,36 @@
+#web2.py
+#웹서버와 통신
+import requests
+#크롤링
+from bs4 import BeautifulSoup
+
+#당근마켓
+url = "https://www.daangn.com/fleamarket/"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text,"html.parser")
+f = open("dangn.txt","wt", encoding="utf-8")
+
+posts = soup.find_all("div",attrs={"class":"card-desc"})
+for post in posts:
+    title = post.find("h2",attrs={"class":"card-title"})
+    price = post.find("div",attrs={"class":"card-price"})
+    addr = post.find("div",attrs={"class":"card-region-name"})
+
+    title = title.text.replace("\n","").strip()
+    price = price.text.replace("\n","").strip()    
+    addr = addr.text.replace("\n","").strip()    
+    
+    print("{0},{1},{2}".format(title, price, addr).strip())
+    f.write(f"{title},{price},{addr}\n")
+
+f.close()
+
+# <div class="card-desc">
+#       <h2 class="card-title">네이버페이44만원 싸게팝니다</h2>
+#       <div class="card-price ">
+#         400,000원
+#       </div>
+#       <div class="card-region-name">
+#         경기도 부천시 춘의동
+#       </div>
